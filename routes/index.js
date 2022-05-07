53fd8eb216e1e1168b7eb1cb092ddffc56c9185c
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-07 17:40:55 +0200

    iteration 3, pending styling

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
-router.get('/movies', (req, res, next) => res.render('movies'))
+router.get('/movies', async (req, res, next) => {
+    try{
+        const response = await MovieModel.find().select('title image')
+        res.render('movies', {
+            movies: response
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+})
 
 module.exports = router;
